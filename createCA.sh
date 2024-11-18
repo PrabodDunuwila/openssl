ea01bff6c23903ee558c4d0c158742268b45dbec
PrabodDunuwila <praboddunuwila@gmail.com>
2024-11-19 00:42:07 +0530

    update create CA

--- createCA.sh
+++ createCA.sh
@@ -1,5 +1,3 @@
-#!/bin/sh
-
 echo "Setting up the directories"
 mkdir ./sslCA
 echo "sslCA"
@@ -10,6 +8,7 @@ echo "sslCA/private"
 mkdir ./sslCA/newcerts
 echo "sslCA/newcerts"
 
+echo "Initialize the CA"
 cd ./sslCA
 echo 1000 > serial
 touch index.txt
@@ -18,12 +17,10 @@ echo "Creating the CA"
 echo "Use a strong password and keep it safe!"
 openssl req -new -newkey rsa:2048 -x509 -days 3650 -extensions v3_ca -keyout private/cakey.pem -out cacert.pem -config ../ca.cnf
 
+echo "Review the generated files"
 more ./cacert.pem
 more ./private/cakey.pem
 
 echo "sslCA/cacert.pem"
 echo "sslCA/private/cakey.pem"
-
-echo "All Done!"
-
-
+echo "CA created!!!"
